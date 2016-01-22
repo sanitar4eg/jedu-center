a4531f1047e6390ce6b1ad2a12efab677d6568b3
Denis <sanitar.92@gmail.com>
2016-01-22 12:05:54 +0300

    Removed French language

--- src/main/webapp/scripts/components/language/language.service.js
+++ src/main/webapp/scripts/components/language/language.service.js
@@ -27,7 +27,7 @@ angular.module('jeducenterApp')
  They are written in English to avoid character encoding issues (not a perfect solution)
  */
     .constant('LANGUAGES', [
-        'en', 'fr'
+        'en'
         ,'ru'
         // jhipster-needle-006 - JHipster will add new languages here
     ]
