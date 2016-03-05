3a04a7a8a38eb55bf7c2e386f47f11d1dc4098e3
Denis <sanitar.92@gmail.com>
2016-03-06 01:07:50 +0300

    * Create form for export students

--- src/main/webapp/scripts/app/entities/student/integration/student.integration.controller.js
+++ src/main/webapp/scripts/app/entities/student/integration/student.integration.controller.js
@@ -1,15 +1,30 @@
 'use strict';
 
 angular.module('jeducenterApp')
-    .controller('StudentIntegrationController', function ($scope, $state, $location,  StudentIntegration) {
-
-        $scope.refresh = function () {
-
-        };
+    .controller('StudentIntegrationController', function ($scope, $state, $location, $http, tmhDynamicLocale,  StudentIntegration) {
 
         $scope.getImportFile = function () {
             var importUrl = $location.protocol()+"://"+$location.host()+":"+$location.port()+"/api/import/students/";
             window.open(importUrl);
+        };
+
+        var inputFile = $("#input-file").fileinput({
+            language: tmhDynamicLocale.get()
+        });
+
+        $scope.uploadExportFile=function (){
+            var formData=new FormData();
+            formData.append("file",inputFile.get(0).files[0]);
+            $http({
+                method: 'POST',
+                url: "api/export/students/",
+                headers: {'Content-Type': undefined},
+                data: formData,
+                transformRequest: function(data, headersGetterFunction) {
+                    return data;
+                }
+            });
+
         };
 
     });
