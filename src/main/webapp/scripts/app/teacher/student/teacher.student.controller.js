b114a5a0a34af98c2a2f7c008322be9157db0f38
Denis <sanitar.92@gmail.com>
2016-04-14 01:07:56 +0300

    * Added temp ui-grid table

--- src/main/webapp/scripts/app/teacher/student/teacher.student.controller.js
+++ src/main/webapp/scripts/app/teacher/student/teacher.student.controller.js
@@ -4,12 +4,35 @@ angular.module('jeducenterApp')
     .controller('TeacherStudentController', function ($scope, $state, Student) {
 
         $scope.students = [];
-        $scope.loadAll = function() {
-            Student.query(function(result) {
-               $scope.students = result;
+        $scope.myData = [];
+        $scope.loadAll = function () {
+            Student.query(function (result) {
+                $scope.students = result;
+                $scope.myData = result;
+                $scope.studentsGrid.data = result;
             });
         };
+
         $scope.loadAll();
+
+        $scope.studentsGrid = {
+            // enableSorting: false,
+            enableGridMenu: true,
+            enableColumnResizing: true,
+            columnDefs: [
+                {name: 'Имя', field: 'firstName', width: '30%'},
+                {name: 'Фамилия', field: 'lastName'},
+                {name: 'Тип обучения', field: 'type'},
+                {name: 'Email', field: 'email'},
+                {name: 'Телефон', field: 'phone'},
+                {name: 'Университет', field: 'university'},
+                {name: 'Специальность', field: 'specialty'},
+                {name: 'Курс', field: 'course'},
+                {name: 'Активен', field: 'isActive'}
+            ]
+
+            // columnDefs: [{name: 'Имя', field: 'firstName'}],
+        };
 
 
         $scope.refresh = function () {
