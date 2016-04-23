c5c818e0e56426b479a7e27ed9967076b1dc15e0
Denis <sanitar.92@gmail.com>
2016-04-23 12:13:48 +0300

    * Fixed translation for ui-grid table

--- src/main/webapp/scripts/app/teacher/student/teacher.student.controller.js
+++ src/main/webapp/scripts/app/teacher/student/teacher.student.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('jeducenterApp')
     .controller('TeacherStudentController', function ($scope, $state, Student, tmhDynamicLocale,
-                                                      i18nService) {
+                                                      i18nService, $translate, $log) {
 
         $scope.students = [];
         $scope.loadAll = function () {
@@ -20,51 +20,57 @@ angular.module('jeducenterApp')
         $scope.studentsGrid = {
             enableGridMenu: true,
             enableColumnResizing: true,
+            gridMenuTitleFilter: $translate,
             columnDefs: [
                 {
-                    field: 'firstName', width: '10%',
-                    headerCellTemplate: '<span translate="jeducenterApp.student.firstName"/>'
+                    displayName: 'jeducenterApp.student.firstName', field: 'firstName', width: '10%',
+                    headerCellFilter: "translate"
                 },
                 {
-                    field: 'middleName', width: '12%',
-                    headerCellTemplate: '<span translate="jeducenterApp.student.middleName"/>'
+                    displayName: 'jeducenterApp.student.middleName', field: 'middleName', width: '12%',
+                    headerCellFilter: "translate"
                 },
                 {
-                    field: 'lastName', width: '12%',
-                    headerCellTemplate: '<span translate="jeducenterApp.student.lastName"/>'
+                    displayName: 'jeducenterApp.student.lastName', field: 'lastName', width: '12%',
+                    headerCellFilter: "translate"
                 },
                 {
-                    field: 'type', width: '8%',
-                    headerCellTemplate: '<span translate="jeducenterApp.student.type"/>'
+                    displayName: 'jeducenterApp.student.type', field: 'type', width: '8%',
+                    headerCellFilter: "translate"
                 },
                 {
-                    field: 'email', width: '14%',
-                    headerCellTemplate: '<span translate="jeducenterApp.student.email"/>'
+                    displayName: 'jeducenterApp.student.email', field: 'email', width: '14%',
+                    headerCellFilter: "translate"
                 },
                 {
-                    field: 'phone', width: '11%',
-                    headerCellTemplate: '<span translate="jeducenterApp.student.phone"/>'
+                    displayName: 'jeducenterApp.student.phone', field: 'phone', width: '11%',
+                    headerCellFilter: "translate"
                 },
                 {
-                    field: 'university', width: '10%',
-                    headerCellTemplate: '<span translate="jeducenterApp.student.university"/>'
+                    displayName: 'jeducenterApp.student.university', field: 'university', width: '10%',
+                    headerCellFilter: "translate"
                 },
                 {
-                    field: 'specialty', width: '12%', visible: false,
-                    headerCellTemplate: '<span translate="jeducenterApp.student.specialty"/>'
+                    displayName: 'jeducenterApp.student.specialty', field: 'specialty', width: '12%', visible: false,
+                    headerCellFilter: "translate"
                 },
                 {
-                    field: 'course', width: '4%', visible: false,
-                    headerCellTemplate: '<span translate="jeducenterApp.student.course"/>'
+                    displayName: 'jeducenterApp.student.course', field: 'course', width: '4%', visible: false,
+                    headerCellFilter: "translate"
                 },
                 {
-                    field: 'isActive', width: '8%', type: 'boolean',
+                    displayName: 'jeducenterApp.student.isActive', field: 'isActive', width: '8%', type: 'boolean',
                     headerCellTemplate: '<span translate="jeducenterApp.student.isActive"/>'
                 },
                 {
-                    name: 'groupOfStudent', width: '8%', visible: false,
-                    headerCellTemplate: '<span translate="jeducenterApp.student.groupOfStudent"/>',
+                    displayName: 'jeducenterApp.student.groupOfStudent', name: 'groupOfStudent', width: '8%', visible: false,
+                    headerCellFilter: "translate",
                     cellTemplate: 'scripts/app/teacher/student/ui-grid/student.group.cell.html'
+                },
+                {
+                    displayName: 'jeducenterApp.student.curator', name: 'curator', width: '8%', visible: false,
+                    headerCellFilter: "translate",
+                    cellTemplate: 'scripts/app/teacher/student/ui-grid/student.curator.cell.html'
                 },
                 {
                     name: ' ', width: '10%',
