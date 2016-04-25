c5423a38681884d889a49dfa7ee1fb74ea0f81d9
Denis <sanitar.92@gmail.com>
2016-04-25 12:26:35 +0300

    * Updated RecallFileResource.java

--- src/main/java/edu/netcracker/center/web/rest/util/RecallFileResource.java
+++ src/main/java/edu/netcracker/center/web/rest/util/RecallFileResource.java
@@ -3,15 +3,16 @@ package edu.netcracker.center.web.rest.util;
 import com.codahale.metrics.annotation.Timed;
 import edu.netcracker.center.domain.Recall;
 import edu.netcracker.center.repository.RecallRepository;
+import edu.netcracker.center.service.FileServerService;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
@@ -26,10 +27,13 @@ import java.io.InputStream;
 public class RecallFileResource {
 
     private final Logger log = LoggerFactory.getLogger(RecallFileResource.class);
-    private final static String RECALL_PATH = "recall/students/";
+    private final static String RECALL_PATH = "/recall/students/";
 
     @Inject
     RecallRepository recallRepository;
+
+    @Inject
+    FileServerService fileServerService;
 
     /**
      * GET  /recalls/file/:id -> get recalls file by "id".
@@ -38,10 +42,11 @@ public class RecallFileResource {
         method = RequestMethod.GET,
         produces = MediaType.APPLICATION_JSON_VALUE)
     @Timed
-    public void getHistoryOfStudents(@PathVariable Long id, HttpServletResponse response) {
+    public void downloadFile(@PathVariable Long id, HttpServletResponse response) {
         log.debug("REST request to get file of Recall by id: {}", id);
         Recall recall = recallRepository.getOne(id);
-        try (InputStream is = new FileInputStream(RECALL_PATH + recall.getFile())) {
+        String filePath = fileServerService.getPath() + RECALL_PATH + recall.getId() + "/" + recall.getFile();
+        try (InputStream is = new FileInputStream(filePath)) {
             IOUtils.copy(is, response.getOutputStream());
             response.flushBuffer();
         } catch (IOException e) {
@@ -57,24 +62,24 @@ public class RecallFileResource {
         method = RequestMethod.POST,
         produces = MediaType.APPLICATION_JSON_VALUE)
     @Timed
-    public void handleImport(@RequestParam("file") MultipartFile file, @PathVariable Long id,
-                             HttpServletRequest request) {
+    @Transactional
+    public void saveFile(@RequestParam("file") MultipartFile file, @PathVariable Long id) {
         Recall recall = recallRepository.getOne(id);
         String fileName = file.getOriginalFilename();
-        recall.setFile(fileName);
-        recallRepository.save(recall);
-        String dirPath = request.getServletContext().getRealPath("/") + RECALL_PATH + id;
+        String dirPath = fileServerService.getPath() + RECALL_PATH + id;
         log.debug("REST request to save file of Recall by id: {}, to path: {}, fileName: {}", id, dirPath,
             fileName);
         try {
             File directory = new File(dirPath);
             boolean result = directory.mkdirs();
             log.debug("Dirs Created: {}", result);
-            file.transferTo(new File(dirPath  + "/" + fileName));
+            file.transferTo(new File(dirPath + "/" + fileName));
         } catch (IOException e) {
             log.error("Error saving file for recall: {}, file:{}", id, fileName, e);
             throw new RuntimeException(e);
         }
+        recall.setFile(fileName);
+        recallRepository.save(recall);
     }
 
 
