561c2587c53ff7d95350109d344e2c71b84392e1
Denis <sanitar.92@gmail.com>
2016-04-25 22:41:21 +0300

    * permitAll for apply form

--- src/main/java/edu/netcracker/center/config/SecurityConfiguration.java
+++ src/main/java/edu/netcracker/center/config/SecurityConfiguration.java
@@ -108,6 +108,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/register").permitAll()
             .antMatchers("/api/activate").permitAll()
             .antMatchers("/api/authenticate").permitAll()
+            .antMatchers("/api/apply/**").permitAll()
             .antMatchers("/api/account/reset_password/init").permitAll()
             .antMatchers("/api/account/reset_password/finish").permitAll()
             .antMatchers("/api/logs/**").hasAuthority(AuthoritiesConstants.ADMIN)
