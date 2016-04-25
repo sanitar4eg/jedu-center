73d8db8e910c0990485138080324a64915ef54c4
Denis <sanitar.92@gmail.com>
2016-04-25 23:36:34 +0300

    * Updated apply page

--- src/main/webapp/scripts/app/ec/apply/apply.controller.js
+++ src/main/webapp/scripts/app/ec/apply/apply.controller.js
@@ -1,6 +1,26 @@
 'use strict';
 
 angular.module('jeducenterApp')
-    .controller('ApplyController', function ($scope, $translate, $timeout, Auth) {
-        
+    .controller('ApplyController', function ($scope, $state, $translate, fileService, FormFile) {
+
+        var onSaveSuccess = function (result) {
+            $scope.$emit('jeducenterApp:formUpdate', result);
+            fileService.setFile(null);
+            console.log('COMPLETED!!!!!');
+            alert($translate.instant("apply.completed"));
+            $state.go('home');
+        };
+
+        var onSaveError = function (result) {
+            $scope.isSaving = false;
+        };
+
+        $scope.save = function () {
+            var file = fileService.getFile();
+            var formData = new FormData();
+            formData.append("file", file);
+            FormFile.uploadFile(formData, onSaveSuccess, onSaveError);
+
+        };
+
     });
