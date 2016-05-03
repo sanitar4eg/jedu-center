2c01e8360f9fb3f00f3ca1fb16d20448ced4d6aa
Denis <sanitar.92@gmail.com>
2016-05-03 22:49:35 +0300

    * Update service for student's registration

--- src/main/webapp/scripts/components/auth/services/register.students.service.js
+++ src/main/webapp/scripts/components/auth/services/register.students.service.js
@@ -5,13 +5,9 @@ angular.module('jeducenterApp')
         return $resource('api/register/students/:students', {}, {
             'update': {
                 method: 'POST',
-                isArray: true,
-                transformRequest: function (input) {
-                    console.log(JSON.stringify(input));
-                    return input;
-                },
                 transformResponse: function (data) {
                     // data = angular.fromJson(data);
+                    console.log(data);
                     return data;
                 }
             }
