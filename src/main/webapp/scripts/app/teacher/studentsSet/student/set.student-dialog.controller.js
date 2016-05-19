856181f59e5dada061015f6a1cc1f6b45b1bd244
Denis <sanitar.92@gmail.com>
2016-05-20 02:02:08 +0300

    * Fixed translate for type of result

--- src/main/webapp/scripts/app/teacher/studentsSet/student/set.student-dialog.controller.js
+++ src/main/webapp/scripts/app/teacher/studentsSet/student/set.student-dialog.controller.js
@@ -1,15 +1,19 @@
 'use strict';
 
 angular.module('jeducenterApp').controller('SetStudentDialogController',
-    ['$scope', '$stateParams', '$uibModalInstance', '$q', 'entity', 'Student', 'User', 'GroupOfStudent', 'Curator', 'Form', 'StudentsSet', 'LearningType',
-        function($scope, $stateParams, $uibModalInstance, $q, entity, Student, User, GroupOfStudent, Curator, Form, StudentsSet, LearningType) {
+    ['$scope', '$stateParams', '$uibModalInstance', '$q', 'entity', 'Student', 'User', 'GroupOfStudent', 'Curator',
+        'Form', 'StudentsSet', 'LearningType', 'LearningResult',
+        function($scope, $stateParams, $uibModalInstance, $q, entity, Student, User, GroupOfStudent, Curator, Form,
+                 StudentsSet, LearningType, LearningResult) {
 
         $scope.student = entity;
+            console.log($scope.student);
         $scope.groupofstudents = GroupOfStudent.query({isActive: true});
         $scope.curators = Curator.query({isActive: true});
         $scope.forms = Form.query({filter: 'student-is-null', isActive: true});
         $scope.studentssets = StudentsSet.query();
         $scope.learningtypes = LearningType.query();
+        $scope.learningresults = LearningResult.query();
         $q.all([$scope.student.$promise, $scope.forms.$promise]).then(function() {
             if (!$scope.student.form || !$scope.student.form.id) {
                 return $q.reject();
