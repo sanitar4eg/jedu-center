f6a15644cbdf08adb3d30335d2bbe15ee44865c5
Denis <sanitar.92@gmail.com>
2016-06-21 04:19:11 +0300

    * Added curatorial recall

--- src/main/java/edu/netcracker/center/domain/enumeration/TypeRecallEnumeration.java
+++ src/main/java/edu/netcracker/center/domain/enumeration/TypeRecallEnumeration.java
@@ -4,5 +4,5 @@ package edu.netcracker.center.domain.enumeration;
  * The TypeRecallEnumeration enumeration.
  */
 public enum TypeRecallEnumeration {
-    Technical,Interview,Managerial
+    Technical,Interview,Managerial,Curatorial
 }
